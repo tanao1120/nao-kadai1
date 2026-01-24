0890fc031d87c295f3df4ed1d374d286c06bd274
tanao1120 <tanao1120@gmial.com>
2026-01-24 15:25:34 +0900

    不要なファイルの削除

--- src/database/migrations/2026_01_19_235615_create_contacts_table.php
+++ src/database/migrations/2026_01_19_235615_create_contacts_table.php
@@ -17,7 +17,7 @@ class CreateContactsTable extends Migration
         Schema::create('contacts', function (Blueprint $table) {
             $table->id();
 
-            $table->unsignedBigInteger('ccategory_id');
+            $table->unsignedBigInteger('category_id');
 
             $table->string('first_name');
             $table->string('last_name');
